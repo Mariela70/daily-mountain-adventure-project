0d791e6556484ef393fb5a3810ce41657c6d627e
Mariela <marielamiteva97@abv.bg>
2023-03-16 22:12:06 +0200

    Add react router to project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Catalog from './components/Catalog/Catalog';
+
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Detaials from './components/Details/Details';
+import Create from './components/Create/Create';
+import Profile from './components/Profile/Profile';
+import Edit from './components/Edit/Edit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main id="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/" element={<Detaials />} />
+          <Route path="/" element={<Edit />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/profile" element={<Profile />} />
+
+        </Routes>
+      </main>
+    </>
   );
 }
 
