28063949b67be0c2eb066130e8ec2b629bea9e07
Mariela <marielamiteva97@abv.bg>
2023-03-18 21:55:19 +0200

    Show adventure in catalog page

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,25 +1,17 @@
-import { useEffect, useState } from "react";
-import * as adventureService from '../../services/adventureService';
-import './catalog.css';
-import LatestAdventure from "./LatestAdventure/LatestAdventure";
-const Catalog = () => {
-  const [adventures, setAdventure] = useState([]);
 
-  useEffect(() => {
-    adventureService.getAll()
-      .then(result => {
-        setAdventure(result);
-      });
-  }, []);
+import './catalog.css';
+import CatalogItem from './CatalogItem/CatalogItem';
+
+const Catalog = ({adventures}) => {
+  
   return (
   
       <section id="catalog">
         <h1>All posts</h1>
         <div className="band">
-          <h1>Latest Adventure</h1>
 
           {adventures.length > 0
-            ? adventures.map(x => <LatestAdventure key={x._id} adventure={x} />)
+            ? adventures.map(x => <CatalogItem key={x._id} adventure={x} />)
             : <p className="no-offer">There are no posts yet...</p>
           }
         </div>
---
+++ src/services/adventureService.js
@@ -0,0 +1,4 @@
+import * as request from "./requester";
+const baseUrl = 'http://localhost:3030';
+
+export const getAll = () => request.get(`${baseUrl}/data/adventure`);
