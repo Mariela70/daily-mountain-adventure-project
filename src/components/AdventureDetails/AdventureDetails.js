800837415e0fe442a6c4578dac1a655cd75317b7
Mariela <marielamiteva97@abv.bg>
2023-03-27 23:31:08 +0300

    Add comments

--- src/components/AdventureDetails/AdventureDetails.js
+++ src/components/AdventureDetails/AdventureDetails.js
@@ -1,64 +1,119 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import './AdventureDetails.css';
 import * as adventureService from '../../services/adventureService';
-const AdventureDetails =({
+import { AddComment } from "./AddComment/AddComment";
+import * as commentService from '../../services/commentService';
+
+const AdventureDetails = ({
   adventures,
   adventureDelete,
 }) => {
-  const {adventureId} = useParams();
-  const adventure = adventures.find(x => x._id === adventureId);
-  const navigate = useNavigate()
-;
+  const [adventure, setAdventure] = useState({});
+  const { user, isAuthenticated, email} = useContext(AuthContext);
+  const { adventureId } = useParams();
+ 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    Promise.all([
+      adventureService.getOne(adventureId),
+      commentService.getAll(adventureId),
+    ]).then(([adventureData, comments]) => {
+      setAdventure({
+        ...adventureData,
+        comments,
+      });
+    });
+  }, [adventureId]);
+
+
   const adventureDeleteHandler = () => {
     const confirmation = window.confirm('Are you sure you want to delete this adventure?')
 
-    if(confirmation) {
+    if (confirmation) {
       adventureService.remove(adventureId)
-      .then(() => {
-        adventureDelete(adventureId)
-        navigate('/catalog')
-      })
+        .then(() => {
+          adventureDelete(adventureId)
+          navigate('/catalog')
+        })
     }
   }
+  const adventureVoteHandler = () => {
+
+  }
+  const onCommentSubmit = async (values) => {
+   const response = await commentService.create(adventureId, values.comment);
+  
+   setAdventure(state => ({
+    ...state,
+    comment: [
+      ...state.comments,
+      {
+        ...response,
+        author: {
+          email
+        }
+      } 
+    ],
+   }));
+  };
+
+  const isOwner = adventure._ownerId === user._id;
   return (
-        
-  <section id="details-page">
-    <div className="main_card">
-      <div className="card_left">
-        <div className="card_datails">
-          <h1>Title: {adventure.title}</h1>
-          <h3>Created by an author: {adventure.user}</h3>
-          <div className="card_animal">
-            <p className="card-location">Location: {adventure.location}</p>
-            <p className="card-date">Date: {adventure.date}</p>
-          </div>
-          <p className="disc">
-            Description: {adventure.description}
-          </p>
-          {/* If there is no registered user, do not display buttons*/}
-          <div className="social-btn">
-            {/* Only for registered user and author of the post */}
-            <Link to={`/adventures/${adventure._id}/edit`} className="edit-btn">
-              Edit
-            </Link>
-            <button onClick={adventureDeleteHandler} className="del-btn">
-              Delete
-            </button>
-            {/* logged in users, who have not yet voted*/}
-            <Link to="#" className="vote-up">
-              UpVote +1
-            </Link>
-            <Link to="#" className="vote-down">
-              DownVote -1
-            </Link>
-            {/* logged in user who has already voted*/}
-            <p className="thanks-for-vote">Thanks For Voting</p>
+
+    <section id="details-page">
+      <div className="main_card">
+        <div className="card_left">
+          <div className="card_datails">
+            <h1>Title: {adventure.title}</h1>
+            <h3>Created by an author: {user.email}</h3>
+            <div className="card_animal">
+              <p className="card-location">Location: {adventure.location}</p>
+              <p className="card-date">Date: {adventure.date}</p>
+            </div>
+            <p className="disc">
+              Description: {adventure.description}
+            </p>
+
+            <div className="comments">
+              <h2>Comments:</h2>
+              <ul>
+                {adventure.comments && adventure.comments.map(x => (
+                  <li key={x._id} className="comment">
+                    <p>{x.author.email}: {x.comment}</p>
+                  </li>
+                ))}
+              </ul>
+              {!adventure.comments?.length && (
+                <p className="no-comments-yet">No comments</p>
+              )}
+            </div>
+
+            {isOwner && 
+              <div className="social-btn">
+
+                <Link to={`/adventures/${adventure._id}/edit`} className="edit-btn">
+                  Edit
+                </Link>
+                <button onClick={adventureDeleteHandler} className="del-btn">
+                  Delete
+                </button>
+              </div>
+            }
+            {isAuthenticated && <AddComment onCommentSubmit={onCommentSubmit} />}
+
+            <button onClick={adventureVoteHandler} className="vote">
+                  Vote
+                </button>
+              
+            
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-    );
+  );
 };
 export default AdventureDetails;
--- src/services/commentService.js
+++ src/services/commentService.js
@@ -2,6 +2,18 @@ import * as request from "./requester";
 
 const baseUrl = 'http://localhost:3030/data/comments';
 
-export const create = (adventureId, comment) => {
-request.post(baseUrl, {adventureId, text: comment})
+export const getAll = async(adventureId) => {
+    const query = encodeURIComponent(`adventureId="${adventureId}"`);
+    const relationQuery = encodeURIComponent(`author=_ownerId:users`);
+
+    const result = await request.get(`${baseUrl}?where=${query}&load=${relationQuery}`);
+    const comments = Object.values(result);
+
+    return comments;
+};
+
+export const create = async (adventureId, comment) => {
+const result = await request.post(baseUrl, {adventureId, comment});
+
+return result;
 };
