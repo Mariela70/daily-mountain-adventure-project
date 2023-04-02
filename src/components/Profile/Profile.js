8d94c772d9fba659b9e1ae8e4590abca330e9390
Mariela <marielamiteva97@abv.bg>
2023-04-02 15:38:01 +0300

    updated Profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,26 +1,15 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext} from "react";
 import { AuthContext } from "../../contexts/AuthContext";
-import * as adventureService from '../../services/adventureService';
 import ProfileItem from "./ProfileItem";
-import { useParams } from "react-router-dom";
 import './profile.css';
 
-const Profile = () => {
+const Profile = ({adventures}) => {
   const { user } = useContext(AuthContext);
-  const { adventureId } = useParams();
-  const [adventures, setAdventure] = useState([]);
 
-  useEffect(() => {
-    adventureService.getMyAdventure(adventureId)
-      .then(result => {
-        setAdventure(result);
-      });
-  }, [adventureId]);
   return (
     <section id="my-posts">
       <h1>My post.</h1>
       <div className="my-container">
-        <h1 className="user">Full Name: {user.firstname} {user.lastName}</h1>
         <h1 className="user-email">Email: {user.email}</h1>
 
         <article className="user-publications">
@@ -28,7 +17,7 @@ const Profile = () => {
             <h3 className="created">Created publications: {adventures.length}</h3>
             {adventures.length > 0
               ? adventures.map(x => <ProfileItem key={x._id} adventure={x} />)
-              : <p className="no-publication">There are no created publications yet...</p>
+              : <p className="no-offer">There are no created publications yet...</p>
             }
           </ul>
         </article>
--- src/components/Profile/ProfileItem.js
+++ src/components/Profile/ProfileItem.js
@@ -5,10 +5,17 @@ const ProfileItem = ({
     adventure,
 }) => {
     return (
-        <article className="adventure">
-            <img className="adventure-img" src={adventure.imageUrl} alt={adventure.title} />
-            <Link className="link-details" to={`/catalog/${adventure._id}`}>Details</Link>
-        </article>
+        <div className="post">
+      <div className="container">
+        <img src={adventure.imageUrl} />
+        <div className="info">
+          <h2>{adventure.title}</h2>
+        </div>
+      </div>
+      <Link to={`/catalog/${adventure._id}`} className="details-btn">
+        Details
+      </Link>
+    </div>
     );
 }
 
